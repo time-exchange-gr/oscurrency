da001c7e3598bcf5d91f1a8c97301eabd0a93e05
Vasili <vasili@techcollective.com>
2011-12-13 02:55:14 +0200

    decided not to change db data during migation

--- db/migrate/20111210011903_disable_forum_notifications.rb
+++ db/migrate/20111210011903_disable_forum_notifications.rb
@@ -1,8 +1,5 @@
 class DisableForumNotifications < ActiveRecord::Migration
-  class MemberPreference < ActiveRecord::Base
-  end
   def self.up
-    MemberPreference.update_all({:forum_notifications => false})
     change_column_default(:member_preferences, :forum_notifications, false)
   end
 
