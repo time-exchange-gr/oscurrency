704d256737139cb8311aef11119ca8f4746d5248
Vasili <vasili@techcollective.com>
2012-01-24 18:44:24 +0200

    Revert "smtp_port admin preference. default port is 587"
    This reverts commit a0a55d853a3873292e80702c4d49042ff3c3b829.

--- db/migrate/20111227003517_add_smtp_port_to_preferences.rb
+++
@@ -1,9 +0,0 @@
-class AddSmtpPortToPreferences < ActiveRecord::Migration
-  def self.up
-    add_column :preferences, :smtp_port, :integer
-  end
-
-  def self.down
-    remove_column :preferences, :smtp_port
-  end
-end
