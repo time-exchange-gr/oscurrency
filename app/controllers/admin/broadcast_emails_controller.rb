ec89269afe35765e45687d957d97223fc74fad9e
Vasili <vasili@techcollective.com>
2012-01-26 03:52:10 +0200

    back to delayed job

--- app/controllers/admin/broadcast_emails_controller.rb
+++ app/controllers/admin/broadcast_emails_controller.rb
@@ -52,8 +52,7 @@ class Admin::BroadcastEmailsController < ApplicationController
       if @broadcast_email.save
         flash[:notice] = 'Admin::BroadcastEmail was successfully created.'
 #        MailingsWorker.async_send_mailing(:mailing_id => @broadcast_email.id)
-        #Cheepnis.enqueue(@broadcast_email)
-        @broadcast_email.perform
+        Cheepnis.enqueue(@broadcast_email)
 
         format.html { redirect_to(admin_broadcast_email_path(@broadcast_email)) }
         format.xml  { render :xml => @broadcast_email, :status => :created, :location => @broadcast_email }
