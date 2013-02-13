b2a1804e349715ef78f26576beb63f1660016f65
Vasili <vasili@techcollective.com>
2013-02-13 14:59:58 +0200

    not setting default locale in the code anymore, there's now an admin preference

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module Oscurrency
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :gr
+    # config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
